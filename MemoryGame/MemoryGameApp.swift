c8b72570c52eca2dd7a0c1b04eaa5b20a3e7bea0
daisiperez326@gmail.com <daisiperez326@gmail.com>
2025-10-30 14:31:51 -0400

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Daisi Perez on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryGameTests/MemoryGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameTests.swift
+//  MemoryGameTests
+//
+//  Created by Daisi Perez on 10/30/25.
+//
+
+import Testing
+@testable import MemoryGame
+
+struct MemoryGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
